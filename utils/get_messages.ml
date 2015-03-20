dee8c6e9c3a8f875d17c6388ce1d14603f7fd4bb
Gregory Tsipenyuk <gregt@gt-mba.local>
2015-03-20 22:58:18 +0000

    added filtering on folders

--- utils/get_messages.ml
+++ utils/get_messages.ml
@@ -17,7 +17,89 @@ open Lwt
 open Imaplet
 open Commands
 
-let get_messages path start stop =
+exception InvalidCommand
+
+let parse_start_stop str =
+  let _ = Regex.match_regex str ~regx:"\\([0-9]+\\)\\(:\\([0-9]+\\|*\\)\\)?" in
+  let start = int_of_string (Str.matched_group 1 str) in
+  let stop = 
+    try
+      let stop = Str.matched_group 3 Sys.argv.(2) in
+      if stop = "*" then
+        max_int
+      else
+        int_of_string stop
+    with _ -> start
+  in
+  (start,stop)
+
+let parse_labels str = 
+  let labels = Str.split (Str.regexp ",") str in
+  List.fold_left (fun acc l -> (Regex.dequote l) :: acc) [] labels
+
+let rec args i mbox index labels =
+  if i >= Array.length Sys.argv then
+    mbox, index,labels
+  else
+    match Sys.argv.(i) with 
+    | "-archive" -> args (i+2) Sys.argv.(i+1) index labels
+    | "-index" -> args (i+2) mbox (parse_start_stop Sys.argv.(i+1)) labels
+    | "-labels" -> args (i+2) mbox index (parse_labels Sys.argv.(i+1))
+    | _ -> raise InvalidCommand
+
+let usage () =
+  Printf.fprintf stderr "usage: get_messages -archive filename -index start[:stop] -folders [folder1,folder2]\n%!"
+
+let commands f =
+  try 
+    let mbox,index,labels = args 1 "" (1,max_int) [] in
+    if mbox = "" then
+      raise InvalidCommand
+    else
+      try 
+        f mbox index labels
+      with ex -> Printf.printf "%s\n%!" (Printexc.to_string ex)
+  with _ -> usage ()
+
+let mailbox_of_gmail_label message = 
+  if Regex.match_regex message ~regx:"^X-Gmail-Labels: \\(.+\\)$" = false then 
+    ("INBOX")
+  else (
+    let labels = Str.split (Str.regexp ",") (Str.matched_group 1 message) in
+    let (label,inferred) =
+    List.fold_left (fun (label,inferred) l ->
+      match l with 
+      | "Important" -> (label,inferred)
+      | "Starred" -> (label,inferred)
+      | "Sent" -> (label,(Some "Sent Messages"))
+      | "Trash" -> (label,(Some "Deleted Messages"))
+      | "Unread" -> (label,inferred)
+      | "Draft" -> (label,(Some "Drafts"))
+      | label -> ((Some label),inferred)
+    ) (None,None) labels
+    in
+    match label with
+    | None ->
+      begin
+      match inferred with
+      | None -> ("INBOX")
+      | Some label -> (label)
+      end
+    | Some label ->
+      let label = Regex.replace ~regx:"[Imap]/" ~tmpl:"" label in
+      let label = Regex.replace ~case:false ~regx:"inbox" ~tmpl:"INBOX" label in
+      (label)
+  )
+
+let filtered message labels = 
+  if labels = [] then
+    false
+  else (
+    let label = mailbox_of_gmail_label message in
+    (List.exists (fun l -> l = label) labels) = false
+  )
+
+let get_messages path start stop labels =
   Lwt_io.with_file ~mode:Lwt_io.Input path (fun ic ->
     let buffer = Buffer.create 10000 in
     let rec loop ic buffer cnt = 
@@ -33,11 +115,18 @@ let get_messages path start stop =
       let regx = "^from [^ ]+ " ^ Regex.dayofweek ^ " " ^ Regex.mon ^ " " ^
         "[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9] [0-9]+" in
       if Regex.match_regex line ~case:false ~regx && Buffer.length buffer > 0 then (
-        if cnt >= start then
-          Printf.printf "%s%!" (Buffer.contents buffer);
+        let message = Buffer.contents buffer in
+        let counting = 
+        if filtered message labels = false then (
+          if cnt >= start then
+            Printf.printf "%s%!" message;
+          true
+        ) else
+          false
+        in
         Buffer.clear buffer;
         Buffer.add_string buffer line;
-        loop ic buffer (cnt+1)
+        loop ic buffer (if counting then (cnt+1) else cnt)
       ) else (
         Buffer.add_string buffer line;
         loop ic buffer cnt
@@ -48,15 +137,8 @@ let get_messages path start stop =
   )
 
 let () =
-  Lwt_main.run (
-    let path = Sys.argv.(1) in
-    let _ = Regex.match_regex Sys.argv.(2) ~regx:"\\([0-9]+\\)\\(:\\([0-9]+\\)\\)?" in
-    let start = int_of_string (Str.matched_group 1 Sys.argv.(2)) in
-    let stop = 
-      try
-        let stop = Str.matched_group 3 Sys.argv.(2) in
-        int_of_string stop
-      with _ -> start
-    in
-    get_messages path start stop
+  commands (fun mbox (start,stop) labels ->
+    Lwt_main.run (
+      get_messages mbox start stop labels
+    )
   )
