b170c096d09ca7b0b7bff14e85e56502fdbd22a8
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-05-14 14:34:55 +0100

    oasis related

--- lib/commands/install.ml
+++ lib/commands/install.ml
@@ -2,4 +2,4 @@
   let bin_path = "/usr/local/bin"
   let users_path = "/usr/local/share/imaplet/users"
   let config_path = "/usr/local/share/imaplet/imaplet.cf"
-  let smtp_srv_exec = "/usr/local/bin/imaplet_smtp"
+  let smtp_srv_exec = "/usr/local/bin/smtplet"
