2026d9772878239fe7d7dce0a2e5bb65540d3468
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-07-24 18:58:04 +0100

    add print message count

--- test/dedup.ml
+++ test/dedup.ml
@@ -10,7 +10,8 @@ let dedup = ref MapStr.empty
 let () =
   Lwt_main.run (
     let config = {Server_config.srv_config with compress = false;compress_attach = false; encrypt = false } in
-    Utils.fold_email_with_file1 Sys.argv.(1) (fun _ message ->
+    Utils.fold_email_with_file1 Sys.argv.(1) (fun cnt message ->
+      Printf.printf "message %d\n%!" cnt;
       Ssl_.get_system_keys Server_config.srv_config >>= fun (pub,_) ->
       Email_parse.parse pub config message ~save_message:(fun _ _ _ _ _ -> return ())
       ~save_attachment:(fun _ _ attachment -> 
@@ -18,14 +19,14 @@ let () =
         if MapStr.exists (fun k _ -> k = hash) !dedup then (
           let (v,s,c) = MapStr.find hash !dedup in
           dedup := MapStr.add hash (v+1,s,c) !dedup;
-          Printf.printf "found duplicate %s,%d\n" hash (v+1)
+          Printf.printf "found duplicate %s,%d,%d\n" hash (v+1) s
         ) else (
           let comps = String.length (Imap_crypto.do_compress ~header:true attachment) in
           dedup := MapStr.add hash (1, String.length attachment,comps) !dedup
         );
         return ()
-      )
-    ) () >>= fun () ->
+      ) >> return (cnt+1)
+    ) 1 >>= fun _ ->
     let (dup,total,ctotal) = MapStr.fold (fun k (cnt,size,comps) (dup,total,ctotal) ->
       if cnt > 1 then (
         (dup+1,total+size*(cnt-1),ctotal+comps*(cnt-1))
