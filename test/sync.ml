f29cd8b353942dd1d062436cfd08a15624149ef9
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-08-16 14:46:43 +0100

    use custom merge

--- test/sync.ml
+++ test/sync.ml
@@ -1,19 +1,40 @@
 open Lwt
 open Irmin_unix
 
-(*
- * for the test to work:
+(* for the test to work:
  * the -remote-repo is: git://192.168.2.2/var/mail/accounts/test/repo
  * the -local-repo is: /var/mail/accounts/test/repo
- * create git-daemon-export-ok in .git
  * run git daemon on the remote as
- * git daemon --reuseaddr --base-path=/ /
+ * git daemon --reuseaddr --export-all --base-path=/ /
  *)
+
 exception InvalidCommand
 
-let option_val = function
-  | None -> assert(false)
+let opt_val = function
+  | None -> "nil"
   | Some v -> v
+
+let opt_eq x = function
+  | None -> None = x
+  | Some v -> v = x
+
+module ImapContents = 
+  struct
+    include Irmin.Contents.String
+    let merge path ~old x y =
+      let open Irmin.Merge.OP in
+      Printf.printf "merging path: %s %b\n%!" (String.concat "/" path) (x = y);
+      old () >>= function
+      | `Conflict _ -> ok y
+      | `Ok old ->
+      if opt_eq x old then (
+        ok y
+      ) else if opt_eq y old then (
+        ok x
+      ) else (
+        ok y 
+      )
+  end
 
 let rec args i remote local depth =
   if i >= Array.length Sys.argv then
@@ -25,34 +46,26 @@ let rec args i remote local depth =
     | "-depth" -> args (i+2) remote local (Some (int_of_string Sys.argv.(i+1)))
     | _ -> raise InvalidCommand
 
-let usage () =
-  Printf.printf "usage: sync -remote-repo [url] -local-repo [path] [-depth x] \n%!"
-
 let commands f =
   try 
     let remote,local,depth = args 1 None None None in
     if remote = None || local = None then
       raise InvalidCommand;
-      try 
-        f (option_val remote) (option_val local) depth
-      with ex -> Printf.printf "%s\n" (Printexc.to_string ex)
-  with _ -> usage ()
-
-let store = Irmin.basic (module Irmin_git.FS) (module Irmin.Contents.String)
+    f (opt_val remote) (opt_val local) depth
+  with _ ->
+    Printf.printf "usage: sync -remote-repo [url] -local-repo [path] [-depth x] \n%!"
 
 let fetch remote local depth =
-  let config = Irmin_git.config ~root:local ~bare:false () in
+  let store = Irmin.basic (module Irmin_git.FS) (module ImapContents) in
+  let config = Irmin_git.config ~root:local ~bare:true () in
   Irmin.create store config task >>= fun t ->
   let upstream = Irmin.remote_uri remote in
-  Irmin.pull_exn (t "Syncing with upstream store") ?depth upstream `Update >>= fun ()
-->
-  Irmin.read_exn (t "get the INBOX metadata") ["imaplet";"test";"mailboxes";"INBOX";"meta"]>>= fun readme ->
-  Printf.printf "%s\n%!" readme;
-  return_unit
+  Irmin.pull_exn (t "Syncing with upstream store") ?depth upstream `Merge
 
 let () =
   commands (fun remote local depth ->
     Lwt_main.run (
-      fetch remote local depth
+      catch (fun () -> fetch remote local depth)
+        (fun ex -> Printf.printf "----- exception %s\n%!" (Printexc.to_string ex); return ())
     )
   )
