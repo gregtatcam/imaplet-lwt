20532288fa69437bf22e7428c7d531fe663890d4
Gregory Tsipenyuk <gregtsip@gmail.com>
2015-12-25 10:22:56 -0500

    remove message from the entity

--- test/test_lightparse.ml
+++ test/test_lightparse.ml
@@ -4,8 +4,7 @@ open Imaplet
 open Commands
 open Lightparsemail
 
-let pr ent msg = 
-  let message = ent.message in
+let pr message ent msg = 
   let len = ent.size in
   let offset = ent.offset in
   let prfx = if len > 50 then String.sub message offset 50 else
@@ -13,27 +12,27 @@ let pr ent msg =
   let pstfx = if len > 100 then String.sub message (offset+len - 50) 50 else "" in
   Printf.printf "  %s: %d %s...%s\n%!" msg ent.size prfx pstfx
 
-let rec walk (email:lightmail) =
-  pr email.headers.position "Headers";
+let rec walk message (email:lightmail) =
+  pr message email.headers.position "Headers";
   match email.body.content with
-  | `Data -> pr email.body.position "Data"
-  | `Message m -> pr email.body.position "Message"; walk m
-  | `Multipart l -> pr email.body.position "Multipart";
+  | `Data -> pr message email.body.position "Data"
+  | `Message m -> pr message email.body.position "Message"; walk message m
+  | `Multipart l -> pr message email.body.position "Multipart";
     List.iteri (fun cnt (email:lightmail) ->
-      pr email.position (Printf.sprintf "Part %d" cnt);
-      walk email
+      pr message email.position (Printf.sprintf "Part %d" cnt);
+      walk message email
     ) l 
 
 let () =
   Lwt_main.run (
     Utils.fold_email_with_file Sys.argv.(1) (fun cnt message ->
       Printf.fprintf stderr "%d\r%!" cnt;
-      Lightparsemail.Message.parse message >>= fun (light:lightmessage) ->
+      Message.parse message >>= fun (light:lightmessage) ->
       Printf.printf "--> start\n%!";
       Printf.printf "Full message %d\n%!" light.position.size;
-      pr light.postmark "Postmark";
-      pr light.email.position "Email";
-      walk light.email;
+      pr message light.postmark "Postmark";
+      pr message light.email.position "Email";
+      walk message light.email;
       Printf.printf "<-- end\n%!";
       return (`Ok (cnt+1))
     ) 1 >>= fun _ ->
